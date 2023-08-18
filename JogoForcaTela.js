00d1a797f054c728ca4a41bfef430366c23c330a
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-08-18 17:22:49 -0300

    correções no css

--- JogoForcaTela.js
+++ JogoForcaTela.js
@@ -28,7 +28,7 @@ export class JogoForcaTela {
     }
 
     criarTeclado() {
-        let alfabeto = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z".split(',');
+        let alfabeto = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,x,z".split(',');
         
         alfabeto.forEach(l => {
             let botao = document.createElement("button");
